cb6805ef23937fd1638e79e527322a3dcc0ffd8b
adletads <adletadvertisements@gmail.com>
2024-12-15 23:51:40 +0530

    third commit

--- src/data/team.ts
+++ src/data/team.ts
@@ -3,14 +3,14 @@ export const team = [
     id: '1',
     name: 'Jinu Joy',
     role: 'Co Founder ',
-    bio: 'Visionary leader with over 15 years of experience in advertising and marketing.',
+    bio: "An innovative and technology-enthusiast with a passion for creating and exploring. Constantly delving into the latest advancements and tools, always eager to learn and push the boundaries of what's possible.",
     image: 'https://i.imghippo.com/files/kRG2242Kg.jpg'
   },
   {
     id: '2',
     name: 'Sonu S',
     role: 'Co Founder',
-    bio: 'Award-winning designer with a passion for creating impactful advertising campaigns.',
+    bio: 'A dedicated enthusiast of technology and electronics, always keen to explore the latest innovations and utilize cutting-edge tools. Driven by creativity and curiosity, continually forging new paths in the world of tech.',
     image: 'https://i.imghippo.com/files/puCo1151yw.jpg'
   }
 ];
